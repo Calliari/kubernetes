c3ed7bfe388d92f314013d2ec03f0eb969279d40
Caliari Jr <Calliari@users.noreply.github.com>
2020-04-20 20:26:11 +0100

    Update kubernetes-install-master-and-nodes-script.sh

--- kubernetes-install-master-and-nodes-script.sh
+++ kubernetes-install-master-and-nodes-script.sh
@@ -27,7 +27,7 @@ sudo apt-mark hold kubelet kubeadm kubectl
 sudo apt-get update -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 ### Add Docker apt repository.
-add-apt-repository \
+sudo add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) \
   stable"
