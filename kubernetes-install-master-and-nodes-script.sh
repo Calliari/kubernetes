7db682b1ca1deba65d9c4ab37eb38483df35c21f
Caliari Jr <Calliari@users.noreply.github.com>
2020-04-20 19:33:00 +0100

    Update kubernetes-install-master-and-nodes-script.sh

--- kubernetes-install-master-and-nodes-script.sh
+++ kubernetes-install-master-and-nodes-script.sh
@@ -2,7 +2,7 @@
 
 ### Install all these packages on all all servers (Masters and worker-modes)
 
-sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent gnupg2 software-properties-common
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates gnupg2
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
